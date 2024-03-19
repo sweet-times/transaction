de1e9fbf554368214496e0f0121cda408f4f5b07
sweet-times <163997078+sweet-times@users.noreply.github.com>
2024-03-19 16:28:27 +0700

    Add files via upload

---
+++ format.js
@@ -0,0 +1,35 @@
+// Import Database
+import {
+    rawProductList,
+    rawPriceList
+} from './database.js';
+
+// Product List
+export const productList = rawProductList.split('|');
+console.log(productList);
+
+// Price List
+// Split Each Row
+const priceData = rawPriceList.split('>');
+
+let priceListFormat = {};
+
+for (let i = 0; i < priceData.length; i++) {
+    // Split Price Code and Price Data
+    const priceCode = priceData[i].split('/')[0];
+    const priceValue = priceData[i].split('/')[1];
+
+    // Split Price Data of Each Item
+    const priceDetails = priceValue.split('|');
+
+    let priceListArray = {};
+
+    for (let j = 0; j < priceDetails.length; j += 2) {
+        priceListArray[priceDetails[j]] = parseInt(priceDetails[j + 1]);
+    }
+
+    priceListFormat[priceCode] = priceListArray;
+}
+
+export const priceList = priceListFormat;
+console.log(priceList);
---
+++ main.js
@@ -0,0 +1,190 @@
+// Import Lists
+import {
+    productList,
+    priceList
+} from './format.js';
+
+// Price Code List
+function priceDropDown() {
+    let priceCodeList = Object.keys(priceList);
+    let selector = document.getElementById('priceCode');
+
+    for (let i = 0; i < priceCodeList.length; i++) {
+        let option = `<option value="${priceCodeList[i]}">${priceCodeList[i]}</option>`;
+        selector.innerHTML += option;
+    }
+}
+
+priceDropDown();
+
+// Product List Table Content
+function addRow(data) {
+    let tableBody = document.getElementById('productListTable');
+
+    for (let i = 0; i < data.length; i++) {
+        let row = `
+            <tr>
+                <td>${data[i]}</td>
+                <td>
+                    <div class="wrapper">
+                        <button class="counter" onclick="
+                            let currentValue = parseInt(document.getElementById('${data[i]}').value);
+                            let newValue = 0;
+                            
+                            if (currentValue > 0)
+                                newValue = currentValue - 1;
+                            else
+                                newValue = currentValue;
+                            
+                            document.getElementById('${data[i]}').value = newValue;">
+                            −
+                        </button>
+                        <input type="text" class="dataInput" id="${data[i]}" value="0">
+                        <button class="counter" onclick="
+                            let currentValue = parseInt(document.getElementById('${data[i]}').value);
+                            let newValue = 0;
+                            
+                            if (currentValue < 999)
+                                newValue = currentValue + 1;
+                            else
+                                newValue = currentValue;
+                            
+                            document.getElementById('${data[i]}').value = newValue;">
+                            +
+                        </button>
+                    </div>
+                </td>
+                <td>
+                    <span class="subtotal" id="${data[i]}Subtotal">
+                        -
+                    </span>
+                </td>
+            </tr>`
+
+            tableBody.innerHTML += row;
+    }
+}
+
+addRow(productList);
+
+// Calculate Price Button On Click
+// Create Temporary Input Area
+function copyText(htmlElement) {
+    let elementText = htmlElement.innerText;
+
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('value', elementText);
+    inputElement.setAttribute('id', 'copytxt')
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    navigator.clipboard.writeText(document.getElementById('copytxt').value);
+    inputElement.parentNode.removeChild(inputElement);
+}
+
+// Assigning On Click Response
+document.querySelector('#calc').onclick =
+function() {
+    // Validate Price Code Selection
+    let priceCode = document.getElementById('priceCode').value;
+
+    if (priceCode == 'Select') {
+        alert('Please select customer price code.');
+        return;
+    }
+
+    // Validate Number Inputs
+    let priceObject = [];
+    let validation = [];
+    let total = 0;
+
+    for (let i = 0; i < productList.length; i++) {
+        let item = productList[i];
+        let quantity = document.getElementById(item).value;
+        
+        // Validate Quantity Inputs
+        let valid = Number.isInteger(parseInt(quantity));
+        validation.push(valid);
+
+        // Missing Price Alert
+        let priceTag = priceList[priceCode][item];
+        
+        if (isNaN(priceTag) && quantity > 0) {
+            alert(`Error: missing price for ${item}.`);
+
+            console.log(priceList[priceCode]);
+
+            // Price Manual Input
+            priceTag = prompt(`Enter price for ${item}:`);
+
+            while (true) {
+                if (isNaN(priceTag) && quantity > 0) {
+                    priceTag = prompt(`Please enter a valid number.`);
+                    continue;
+                } else {
+                    break;
+                }
+            }                  
+        }
+
+        // Price Calculation
+        let price = priceTag * quantity;
+
+        // Price for Unpurchased Unlisted Products
+        if (isNaN(price))
+            price = 0;
+        
+        total += price;
+
+        priceObject.push([item, quantity, price]);
+    }
+
+    // Validate Delivery Fee Input
+    let deliveryFee = document.getElementById('deliveryFee').value;
+    let deliveryFeeValidity = Number.isInteger(parseInt(deliveryFee));
+    validation.push(deliveryFeeValidity);
+
+    // Total Price Calculation including Delivery Fee
+    total += parseInt(deliveryFee);
+
+    // Alert for Number Inputs Validation
+    if (validation.includes(false)) {
+        alert('Please enter valid quantity and delivery fee');
+        return;
+    }
+
+    // Format Total Price to IDR Function
+    const rupiah = (number) => {
+        return new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR'
+        }).format(number);
+    }
+
+    // Display Subtotal Price
+    for (let i = 0; i < priceObject.length; i++) {
+        const subtotalPrice = priceObject[i][2];
+
+        // Display Unpurchased Item as None
+        let subtotalPriceRupiah = 0;
+
+        if (subtotalPrice == 0)
+            subtotalPriceRupiah = '-';
+        else
+            subtotalPriceRupiah = rupiah(subtotalPrice);
+
+        document.getElementById(priceObject[i][0] + 'Subtotal').innerHTML = subtotalPriceRupiah;
+    }
+
+    // Display Total Price
+    const priceRupiah = rupiah(total);
+    document.getElementById('totalPrice').innerHTML = priceRupiah;
+
+    console.log(priceObject);
+    console.log(priceRupiah);
+
+    // Copy Message to Clipboard
+    document.getElementById('message').innerHTML = `Your total price is ${priceRupiah}.`;
+    copyText(document.querySelector('#message'));
+
+    alert('Message has been copied to clipboard.');
+}
